f392bc4d5e392c11e8cd1c8d55215e76621c30b6
scribbler <luckqinwei@hotmail.com>
2012-01-16 17:04:17 +0800

    first commot

---
+++ sqlite/sqlite.h
@@ -0,0 +1,13 @@
+//
+//  sqlite.h
+//  sqlite
+//
+//  Created by Wei Qin on 12-1-16.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface sqlite : NSObject
+
+@end
---
+++ sqlite/sqlite.m
@@ -0,0 +1,23 @@
+//
+//  sqlite.m
+//  sqlite
+//
+//  Created by Wei Qin on 12-1-16.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import "sqlite.h"
+
+@implementation sqlite
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
